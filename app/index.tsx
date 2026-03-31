a85d444aca1510d9c65311e0644447066a51c29e
Skyline-ly <eytaily123@gmail.com>
2026-03-31 13:38:09 +0700

    the right tailwind setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center w-full"
+
+    >
+      <Text className="text-6xl text-blue-400 ">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
